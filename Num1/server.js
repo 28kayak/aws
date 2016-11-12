ed5ebb66f34081aecb7950b31cdecf1a1659ffe7
28kayak <kaya.ota@sjsu.edu>
2016-11-12 02:25:22 -0800

    change port#

--- Num1/server.js
+++ Num1/server.js
@@ -8,9 +8,9 @@ var express = require('express');
 
 
 var app = express();
-var port = 8888;
+var port = 80;
 //if it is Ok to add port number as in check field 
 app.use('/', function(req,res){
     res.end("AMAZON");
 });
-app.listen(port, function(){console.log("http://172.31.14.74:80")});//172.31.14.74:
+app.listen(port, function(){console.log("http://:80")});//172.31.14.74:
